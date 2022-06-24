474c1074c8aca3f0da3f0bd1e69600f5d4e0f0ca
Trung Phạm <minhtrungxyz123@gmail.com>
2022-06-24 15:17:11 +0700

    upp

---
+++ src/HouseWarehouse.Webapp/Controllers/PaymentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HouseWarehouse.Webapp.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/HouseWarehouse.Webapp/Controllers/ProductLikeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HouseWarehouse.Webapp.Controllers
+{
+    public class ProductLikeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
