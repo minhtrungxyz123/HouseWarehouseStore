3b708a9448cdf5ef647a323844d9ab4d1be413f7
trungpv <trungphamvanvn@gmail.com>
2022-06-25 18:19:04 +0700

    up

---
+++ src/Master.Webapp/Controllers/ForgotPasswordController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Master.Webapp.Controllers
+{
+    public class ForgotPasswordController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
