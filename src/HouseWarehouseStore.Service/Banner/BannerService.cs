5ade309214d7fa5445a197e75a4f57c1be01c3ae
trungpv <trungphamvanvn@gmail.com>
2022-07-06 20:55:03 +0700

    up

--- src/HouseWarehouseStore.Service/Banner/BannerService.cs
+++ src/HouseWarehouseStore.Service/Banner/BannerService.cs
@@ -33,5 +33,27 @@ namespace HouseWarehouseStore.Service
 
             return banner;
         }
+
+        //public async Task<List<BannerModel>> GetAll(int Width, int Height)
+        //{
+        //    var banner = from x in _context.Banners
+        //                 where x.Width == Width && x.Height == Height
+        //                 select new BannerModel()
+        //                 {
+        //                     BannerId = x.BannerId,
+        //                     Url = x.Url,
+        //                     BannerName = x.BannerName,
+        //                     Active = x.Active,
+        //                     Content = x.Content,
+        //                     CoverImage = x.CoverImage,
+        //                     GroupId = x.GroupId,
+        //                     Height = x.Height,
+        //                     Soft = x.Soft,
+        //                     Title = x.Title,
+        //                     Width = x.Width
+        //                 };
+
+        //    return banner.ToList();
+        //}
     }
 }
